92a95ed5eaffde9cd9da08c7eed01a85920c70f1
Joe Schultz <jxscad@gan-jxscad-m.fios-router.home>
2021-03-03 06:33:32 -0500

    Initialized headers

---
+++ filePermissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Author: Joe Schultz (jxs6799@rit.edu)
+#
+# Description:  Set correct permissions on certain system/user
+#               files (/etc/passwd, /etc/shadow, $HOME/.ssh, etc.)
+#
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
---
+++ trumper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Author: Joe Schultz (jxs6799@rit.edu)
+#
+# Description:  We will build a (fire)wall. It will be a great (fire)wall.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+// TODO
