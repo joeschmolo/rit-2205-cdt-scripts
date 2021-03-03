2527398eb13b8360e3d75262d91ec7394a38d212
Joe Schultz <jxs6799@rit.edu>
2021-03-03 07:32:34 -0500

    Initialized file

--- thatsNotYours.sh
+++ thatsNotYours.sh
@@ -3,7 +3,9 @@
 # Author: Joe Schultz (jxs6799@rit.edu)
 #
 # Description:  Set correct permissions on certain system/user
-#               files (/etc/passwd, /etc/shadow, $HOME/.ssh, etc.)
+#               files (/etc/passwd, /etc/shadow, $HOME, etc.)
 #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# TODO
--- yeeter.sh
+++ yeeter.sh
@@ -2,7 +2,7 @@
 #
 # Author: Joe Schultz (jxs6799@rit.edu)
 #
-# Description:  We will build a (fire)wall. It will be a great (fire)wall.
+# Description:  Deletes unncessary files (crons) and users (non Grey Team, non Blue team, non service/system account)
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
